321318a234a436078a9bf416d8b18ccf25de3e63
mbill <m.billupalli@eagles.oc.edu>
2019-11-21 23:48:31 -0600

    version 1.3

--- main.cpp
+++ main.cpp
@@ -3,57 +3,65 @@
 #include <omp.h>
 #include <time.h>
 #include <sys/time.h>
-
-static const long Num_To_Add = 10000000;
-static const double Scale = 10.0 / RAND_MAX;
-//Method to calulat sum of a randomly populated array sequentially
-long add_serial(const char *numbers) {
-    long sum = 0;
-    for (long i = 0; i < Num_To_Add ; i++) {
-        sum += numbers[i];
-    }
+#include <pthread.h>
+// size of array
+#define MAX 30
+// maximum number of threads
+#define MAX_THREAD 4
+static const long Array_size = 3000;
+//Method to calulate sum of a randomly populated array sequentially
+long add_serial(const int *numbers) {
+int sum=0;
+    for (long i = 0; i < Array_size ; i++) {
+            sum += numbers[i];
+        }
     return sum;
 }
-long sum;
+
 //Method to calulate sum of a randomly populated array parallel
-long add_parallel(const char *numbers) {
-#pragma omp for reduction(+:sum)
-   for (long i = 0; i < Num_To_Add ; i++) {
-        sum += numbers[i];
+long int add_parallel(const int *numbers) {
+    long int inntersum=0,sum = 0;
+#pragma omp parallel num_threads(3)
+    {
+        int start, Psize= 3;
+        int singleThread ,end;
+        singleThread= Array_size/Psize;
+
+#pragma omp for
+        for (long start = 0; start < Array_size ; start++) {
+            for(long end=0;end<singleThread;end++){
+                inntersum += numbers[start];
+                start=start+1;
+            }
+            sum+=inntersum;
+            inntersum=0;
+        }
     }
     return sum;
 }
 //Main metho
 int main() {
-    char *numbers = static_cast<char *>(malloc(sizeof(long) * Num_To_Add));
-
-    struct timeval start, end;
-    long chunk_size = Num_To_Add / omp_get_max_threads();
-#pragma omp parallel num_threads(omp_get_max_threads())
+     struct timeval start, end;
+    // Code to generate Ramdom number and store into array.
+    int numbers[Array_size], i;
+    for(i = 0; i <  Array_size; i++)
     {
-        int p = omp_get_thread_num();
-        unsigned int seed = (unsigned int) time(NULL) + (unsigned int) p;
-        long chunk_start = p * chunk_size;
-        long chunk_end = chunk_start + chunk_size;
-        for (long i = chunk_start; i < chunk_end; i++) {
-            numbers[i] = (char) (rand_r(&seed) * Scale);
-        }
+        numbers[i] = (rand() % 100)+1;
     }
 
     printf("Timing sequential...\n");
     gettimeofday(&start, NULL);
     long sum_s = add_serial(numbers);
     gettimeofday(&end, NULL);
-    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+    printf("Took  %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
 
     printf("Timing parallel...\n");
     gettimeofday(&start, NULL);
     long sum_p = add_parallel(numbers);
     gettimeofday(&end, NULL);
-    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+    printf("Took %.6f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
 
     printf("Sum serial: %ld\nSum parallel: %ld", sum_s, sum_p);
 
-    free(numbers);
     return 0;
 }
